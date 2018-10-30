b6fde063bf5953e12c01157d73a5e3fdc641e25b
Nicolas DESSENNE <nico.dessenne@gmail.com>
2018-10-30 14:56:41 +0100

    Main TP1 TP2

--- TP1/src/main/MainTP1.java
+++ TP1/src/main/MainTP1.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Arrays;
 
 import glouton.exception.ConstructiveHeuristicsAlgorithmException;
@@ -25,34 +27,96 @@ public class MainTP1 {
 	 * randomF100 = 8989
 	 */
 	public static void main(String[] args) {
-		File f = new File("randomA100.tsp");
+		try {
+			PrintStream o = new PrintStream(new File("MainTP1.txt"));
+			System.setOut(o); 
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		
+		// GET FILE TSP
+		File fa = new File("randomA100.tsp");
+		File fb = new File("randomB100.tsp");
+		File fc = new File("randomC100.tsp");
+		File fd = new File("randomD100.tsp");
+		File fe = new File("randomE100.tsp");
+		File ff = new File("randomF100.tsp");
+		
+		RandomTSP a = ReadFile.readFile(fa);
+		RandomTSP b = ReadFile.readFile(fb);
+		RandomTSP c = ReadFile.readFile(fc);
+		RandomTSP d = ReadFile.readFile(fd);
+		RandomTSP e = ReadFile.readFile(fe);
+		RandomTSP f = ReadFile.readFile(ff);
+		
 		System.out.println("------------------------------------------------------------");
 		System.out.println("Question 2.1");
-		RandomTSP a = ReadFile.readFile(f);
+		System.out.println("Matrice randomA100.tsp");
 		DisplayEdgeWeightSection.displayEdgeWeightSection(a);
+		System.out.println("Matrice randomB100.tsp");
+		DisplayEdgeWeightSection.displayEdgeWeightSection(b);
+		System.out.println("Matrice randomC100.tsp");
+		DisplayEdgeWeightSection.displayEdgeWeightSection(c);
+		System.out.println("Matrice randomD100.tsp");
+		DisplayEdgeWeightSection.displayEdgeWeightSection(d);
+		System.out.println("Matrice randomE100.tsp");
+		DisplayEdgeWeightSection.displayEdgeWeightSection(e);
+		System.out.println("Matrice randomF100.tsp");
+		DisplayEdgeWeightSection.displayEdgeWeightSection(f);
 		System.out.println("------------------------------------------------------------");
-		int[] a1 = new int[] {0,1,2,3,4,5,6,7,8,9};
-		int[] a2 = new int[] {9,8,7,6,5,4,3,2,1,0};
+		int[] path1 = new int[] {0,1,2,3,4,5,6,7,8,9};
+		int[] path2 = new int[] {9,8,7,6,5,4,3,2,1,0};
 
 		System.out.println("------------------------------------------------------------");
 		System.out.println("Question 2.2");
 		try {
-			System.out.println("Evaluation a1 = " + Evaluate.eval(a, a1));
-			System.out.println("Evaluation a2 = " + Evaluate.eval(a, a2));
-		} catch (RandomTSPException e) {
-			e.printStackTrace();
+			System.out.println("Evaluation A-path1 = " + Evaluate.eval(a, path1));
+			System.out.println("Evaluation A-path2 = " + Evaluate.eval(a, path2));
+			System.out.println("Evaluation B-path1 = " + Evaluate.eval(b, path1));
+			System.out.println("Evaluation B-path2 = " + Evaluate.eval(b, path2));
+			System.out.println("Evaluation C-path1 = " + Evaluate.eval(c, path1));
+			System.out.println("Evaluation C-path2 = " + Evaluate.eval(c, path2));
+			System.out.println("Evaluation D-path1 = " + Evaluate.eval(d, path1));
+			System.out.println("Evaluation D-path2 = " + Evaluate.eval(d, path2));
+			System.out.println("Evaluation E-path1 = " + Evaluate.eval(e, path1));
+			System.out.println("Evaluation E-path2 = " + Evaluate.eval(e, path2));
+			System.out.println("Evaluation F-path1 = " + Evaluate.eval(f, path1));
+			System.out.println("Evaluation F-path2 = " + Evaluate.eval(f, path2));
+		} catch (RandomTSPException ex) {
+			ex.printStackTrace();
 		}
 		System.out.println("------------------------------------------------------------");
 
 		System.out.println("------------------------------------------------------------");
 		System.out.println("Question 2.3");
 		System.out.println("Best result for randomA100 is 10659.");
+		System.out.println("Best result for randomB100 is 9234.");
+		System.out.println("Best result for randomC100 is 9529.");
+		System.out.println("Best result for randomD100 is 9108.");
+		System.out.println("Best result for randomE100 is 8899.");
+		System.out.println("Best result for randomF100 is 8989.");
 		try {
 			System.out.println("Random evaluation 1 randomA100 = " + Evaluate.randomEval(a));
 			System.out.println("Random evaluation 2 randomA100 = " + Evaluate.randomEval(a));
 			System.out.println("Random evaluation 3 randomA100 = " + Evaluate.randomEval(a));
-		} catch (RandomTSPException e) {
-			e.printStackTrace();
+			System.out.println("Random evaluation 1 randomB100 = " + Evaluate.randomEval(b));
+			System.out.println("Random evaluation 2 randomB100 = " + Evaluate.randomEval(b));
+			System.out.println("Random evaluation 3 randomB100 = " + Evaluate.randomEval(b));
+			System.out.println("Random evaluation 1 randomC100 = " + Evaluate.randomEval(c));
+			System.out.println("Random evaluation 2 randomC100 = " + Evaluate.randomEval(c));
+			System.out.println("Random evaluation 3 randomC100 = " + Evaluate.randomEval(c));
+			System.out.println("Random evaluation 1 randomD100 = " + Evaluate.randomEval(d));
+			System.out.println("Random evaluation 2 randomD100 = " + Evaluate.randomEval(d));
+			System.out.println("Random evaluation 3 randomD100 = " + Evaluate.randomEval(d));
+			System.out.println("Random evaluation 1 randomE100 = " + Evaluate.randomEval(e));
+			System.out.println("Random evaluation 2 randomE100 = " + Evaluate.randomEval(e));
+			System.out.println("Random evaluation 3 randomE100 = " + Evaluate.randomEval(e));
+			System.out.println("Random evaluation 1 randomF100 = " + Evaluate.randomEval(f));
+			System.out.println("Random evaluation 2 randomF100 = " + Evaluate.randomEval(f));
+			System.out.println("Random evaluation 3 randomF100 = " + Evaluate.randomEval(f));
+		} catch (RandomTSPException ex) {
+			ex.printStackTrace();
 		}
 		System.out.println("------------------------------------------------------------");
 
@@ -60,36 +124,42 @@ public class MainTP1 {
 		System.out.println("Question 3.1");
 		NearestNeighborAlgorithm nna = new NearestNeighborAlgorithm();
 		try {
-			TSPResult res1 = nna.doAlgorithm(a, 0);
-			System.out.println("NearestNeighborAlgo file:randomA100.tsp beginAt:0");
-			System.out.println("Path = " + Arrays.toString(res1.getPath()));
-			System.out.println("Cost = " + res1.getCost());
-			TSPResult res2 = nna.doAlgorithm(a, 10);
-			System.out.println("NearestNeighborAlgo file:randomA100.tsp beginAt:10");
-			System.out.println("Path = " + Arrays.toString(res2.getPath()));
-			System.out.println("Cost = " + res2.getCost());
-			TSPResult res3 = nna.doAlgorithm(a, 50);
-			System.out.println("NearestNeighborAlgo file:randomA100.tsp beginAt:50");
-			System.out.println("Path = " + Arrays.toString(res3.getPath()));
-			System.out.println("Cost = " + res3.getCost());
-			TSPResult res4 = nna.doAlgorithm(a, 75);
-			System.out.println("NearestNeighborAlgo file:randomA100.tsp beginAt:75");
-			System.out.println("Path = " + Arrays.toString(res4.getPath()));
-			System.out.println("Cost = " + res4.getCost());
 			int rand = Method.getRandomNumberInRange(0, 99);
-			TSPResult res5 = nna.doAlgorithm(a, rand);
-			System.out.println("NearestNeighborAlgo file:randomA100.tsp beginAt:" + rand);
-			System.out.println("Path = " + Arrays.toString(res5.getPath()));
-			System.out.println("Cost = " + res5.getCost());
-		} catch (ConstructiveHeuristicsAlgorithmException e) {
-			e.printStackTrace();
+			nna(nna, a, rand);
+			nna(nna, b, rand);
+			nna(nna, c, rand);
+			nna(nna, d, rand);
+			nna(nna, e, rand);
+			nna(nna, f, rand);
+			
+		} catch (ConstructiveHeuristicsAlgorithmException ex) {
+			ex.printStackTrace();
 		}
 
 		System.out.println("------------------------------------------------------------");
-		
-		System.out.println("Question TD2 - 2");
-
-		
 	}
 
+	private static void nna(NearestNeighborAlgorithm nna, RandomTSP rt, int rand) throws ConstructiveHeuristicsAlgorithmException {
+		TSPResult res1 = nna.doAlgorithm(rt, 0);
+		System.out.println("NearestNeighborAlgo file:"+rt.getFileName()+" beginAt:0");
+		System.out.println("Path = " + Arrays.toString(res1.getPath()));
+		System.out.println("Cost = " + res1.getCost());
+		TSPResult res2 = nna.doAlgorithm(rt, 10);
+		System.out.println("NearestNeighborAlgo file:"+rt.getFileName()+" beginAt:10");
+		System.out.println("Path = " + Arrays.toString(res2.getPath()));
+		System.out.println("Cost = " + res2.getCost());
+		TSPResult res3 = nna.doAlgorithm(rt, 50);
+		System.out.println("NearestNeighborAlgo file:"+rt.getFileName()+" beginAt:50");
+		System.out.println("Path = " + Arrays.toString(res3.getPath()));
+		System.out.println("Cost = " + res3.getCost());
+		TSPResult res4 = nna.doAlgorithm(rt, 75);
+		System.out.println("NearestNeighborAlgo file:"+rt.getFileName()+" beginAt:75");
+		System.out.println("Path = " + Arrays.toString(res4.getPath()));
+		System.out.println("Cost = " + res4.getCost());
+		TSPResult res5 = nna.doAlgorithm(rt, rand);
+		System.out.println("NearestNeighborAlgo file:"+rt.getFileName()+" beginAt:" + rand);
+		System.out.println("Path = " + Arrays.toString(res5.getPath()));
+		System.out.println("Cost = " + res5.getCost());
+	}
+	
 }
--- TP1/src/main/MainTP2.java
+++ TP1/src/main/MainTP2.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import glouton.obj.RandomTSP;
 import glouton.obj.TSPResult;
 import glouton.obj.services.RandomTSP.ReadFile;
@@ -12,106 +15,213 @@ import hillclimbing.obj.services.movement.BestNeighborImproving;
 import hillclimbing.obj.services.movement.FirstNeighborImproving;
 import hillclimbing.obj.services.neighborhood.Swaping;
 import hillclimbing.obj.services.neighborhood.TwoOpt;
+import utils.Method;
 
 public class MainTP2 {
 
 	public static void main(String[] args) {
-		File f = new File("randomA100.tsp");
-		RandomTSP a = ReadFile.readFile(f);
+		try {
+			PrintStream o = new PrintStream(new File("MainTP2.txt"));
+			System.setOut(o); 
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
 		
-		
-		
-		
-		System.out.println("------------------------------------------------------------");
-		System.out.println("Question 2 et 4");
-		
-		HillClimbing hillClimbingSwapFirstNeighbor = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(a), a);
-		HillClimbing hillClimbingTwoOptFirstNeighbor = new HillClimbing(new RandomPathInit(), new TwoOpt(), new FirstNeighborImproving(a), a);
-		HillClimbing hillClimbingSwapBestNeighbor = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(a), a);
-		HillClimbing hillClimbingTwoOptBestNeighbor = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(a), a);
-		
-		int[] a1 = new int[] {0,1,2,3,4,5,6,7,8,9};
-		int[] a2 = new int[] {9,8,7,6,5,4,3,2,1,0};
-		
-		
-		
-		int[] swaping = Swaping.swap(a1, 3, 8);
-		int[] twoOpt = TwoOpt.twoOpt(a2, 2, 7);
-		
-		
-		
-		for (int i=0; i< swaping.length; i++) {
-			System.out.println(swaping[i]);
-		}
-		
-		System.out.println("------------------------------------------------------------");
-		
-		TSPResult tspResult1 = hillClimbingSwapFirstNeighbor.doAlgo(a1);
-		
-		for (int i=0; i< tspResult1.getPath().length; i++) {
-			System.out.println(tspResult1.getPath()[i]);
-		}
-		
-		System.out.println("------------------------------------------------------------");
-		
-		TSPResult tspResult12 = hillClimbingSwapBestNeighbor.doAlgo(a1);
-		
-		for (int i=0; i< tspResult12.getPath().length; i++) {
-			System.out.println(tspResult12.getPath()[i]);
-		}
-		
-		System.out.println("------------------------------------------------------------");
-		
-		
-		for (int i=0; i< twoOpt.length; i++) {
-			System.out.println(twoOpt[i]);
-		}
-		
-		System.out.println("------------------------------------------------------------");
-		
-		
-		TSPResult tspResult2 = hillClimbingTwoOptFirstNeighbor.doAlgo(a1);
-		
-		for (int i=0; i< tspResult2.getPath().length; i++) {
-			System.out.println(tspResult2.getPath()[i]);
-		}
-		
-		System.out.println("------------------------------------------------------------");
-		
-		TSPResult tspResult22 = hillClimbingTwoOptBestNeighbor.doAlgo(a1);
-		
-		for (int i=0; i< tspResult22.getPath().length; i++) {
-			System.out.println(tspResult22.getPath()[i]);
-		}
-		
-		
-		System.out.println("------------------------------------------------------------");
-		
+		// GET FILE TSP
+		File fa = new File("randomA100.tsp");
+		File fb = new File("randomB100.tsp");
+		File fc = new File("randomC100.tsp");
+		File fd = new File("randomD100.tsp");
+		File fe = new File("randomE100.tsp");
+		File ff = new File("randomF100.tsp");
 
+		RandomTSP a = ReadFile.readFile(fa);
+		RandomTSP b = ReadFile.readFile(fb);
+		RandomTSP c = ReadFile.readFile(fc);
+		RandomTSP d = ReadFile.readFile(fd);
+		RandomTSP e = ReadFile.readFile(fe);
+		RandomTSP f = ReadFile.readFile(ff);
+
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Question 2");
+
+		// HILLCLIMBING randomA
+		HillClimbing hcASwapFirst = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(a),
+				a);
+		HillClimbing hcATwoOptFirst = new HillClimbing(new RandomPathInit(), new TwoOpt(),
+				new FirstNeighborImproving(a), a);
+		HillClimbing hcASwapBest = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(a),
+				a);
+		HillClimbing hcATwoOptBest = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(a),
+				a);
+
+		// HILLCLIMBING randomB
+		HillClimbing hcBSwapFirst = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(b),
+				b);
+		HillClimbing hcBTwoOptFirst = new HillClimbing(new RandomPathInit(), new TwoOpt(),
+				new FirstNeighborImproving(b), b);
+		HillClimbing hcBSwapBest = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(b),
+				b);
+		HillClimbing hcBTwoOptBest = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(b),
+				b);
+
+		// HILLCLIMBING randomC
+		HillClimbing hcCSwapFirst = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(c),
+				c);
+		HillClimbing hcCTwoOptFirst = new HillClimbing(new RandomPathInit(), new TwoOpt(),
+				new FirstNeighborImproving(c), c);
+		HillClimbing hcCSwapBest = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(c),
+				c);
+		HillClimbing hcCTwoOptBest = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(c),
+				c);
+
+		// HILLCLIMBING randomD
+		HillClimbing hcDSwapFirst = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(d),
+				d);
+		HillClimbing hcDTwoOptFirst = new HillClimbing(new RandomPathInit(), new TwoOpt(),
+				new FirstNeighborImproving(d), d);
+		HillClimbing hcDSwapBest = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(d),
+				d);
+		HillClimbing hcDTwoOptBest = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(d),
+				d);
+
+		// HILLCLIMBING randomE
+		HillClimbing hcESwapFirst = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(e),
+				e);
+		HillClimbing hcETwoOptFirst = new HillClimbing(new RandomPathInit(), new TwoOpt(),
+				new FirstNeighborImproving(e), e);
+		HillClimbing hcESwapBest = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(e),
+				e);
+		HillClimbing hcETwoOptBest = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(e),
+				e);
+
+		// HILLCLIMBING randomF
+		HillClimbing hcFSwapFirst = new HillClimbing(new RandomPathInit(), new Swaping(), new FirstNeighborImproving(f),
+				f);
+		HillClimbing hcFTwoOptFirst = new HillClimbing(new RandomPathInit(), new TwoOpt(),
+				new FirstNeighborImproving(f), f);
+		HillClimbing hcFSwapBest = new HillClimbing(new RandomPathInit(), new Swaping(), new BestNeighborImproving(f),
+				f);
+		HillClimbing hcFTwoOptBest = new HillClimbing(new RandomPathInit(), new TwoOpt(), new BestNeighborImproving(f),
+				f);
+
+		int[] path = new int[100];
+		for (int i = 0; i < path.length; i++) {
+			path[i] = i;
+		}
+		Method.shuffleArray(path);
+
+		System.out.println("------------------------------------------------------------");
+
+		TSPResult res1A = hcASwapFirst.doAlgo(path);
+		TSPResult res2A = hcATwoOptFirst.doAlgo(path);
+		TSPResult res1B = hcBSwapFirst.doAlgo(path);
+		TSPResult res2B = hcBTwoOptFirst.doAlgo(path);
+		TSPResult res1C = hcCSwapFirst.doAlgo(path);
+		TSPResult res2C = hcCTwoOptFirst.doAlgo(path);
+		TSPResult res1D = hcDSwapFirst.doAlgo(path);
+		TSPResult res2D = hcDTwoOptFirst.doAlgo(path);
+		TSPResult res1E = hcESwapFirst.doAlgo(path);
+		TSPResult res2E = hcETwoOptFirst.doAlgo(path);
+		TSPResult res1F = hcFSwapFirst.doAlgo(path);
+		TSPResult res2F = hcFTwoOptFirst.doAlgo(path);
+		System.out.println("randomA100.tsp : ");
+		System.out.println("- SWAP : " + res1A.getCost());
+		System.out.println("- TWOOPT : " + res2A.getCost());
+		System.out.println("randomB100.tsp : ");
+		System.out.println("- SWAP : " + res1B.getCost());
+		System.out.println("- TWOOPT : " + res2B.getCost());
+		System.out.println("randomC100.tsp : ");
+		System.out.println("- SWAP : " + res1C.getCost());
+		System.out.println("- TWOOPT : " + res2C.getCost());
+		System.out.println("randomD100.tsp : ");
+		System.out.println("- SWAP : " + res1D.getCost());
+		System.out.println("- TWOOPT : " + res2D.getCost());
+		System.out.println("randomE100.tsp : ");
+		System.out.println("- SWAP : " + res1E.getCost());
+		System.out.println("- TWOOPT : " + res2E.getCost());
+		System.out.println("randomF100.tsp : ");
+		System.out.println("- SWAP : " + res1F.getCost());
+		System.out.println("- TWOOPT : " + res2F.getCost());
+
+		System.out.println("------------------------------------------------------------");
 		System.out.println("Question 3");
-		
-		RandomPathInit randomPathInit = new RandomPathInit();
-		int[] randomPath = randomPathInit.initPathAsArray(10);
-		
-		
-		for (int i = 0; i < randomPath.length; i++) {
-			System.out.println(randomPath[i]);
-		}
+		System.out.println("------------------------------------------------------------");
+
+		HillClimbing hcASwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), a), new Swaping(),
+				new FirstNeighborImproving(a), a);
+		HillClimbing hcBSwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), b), new Swaping(),
+				new FirstNeighborImproving(b), b);
+		HillClimbing hcCSwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), c), new Swaping(),
+				new FirstNeighborImproving(c), c);
+		HillClimbing hcDSwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), d), new Swaping(),
+				new FirstNeighborImproving(d), d);
+		HillClimbing hcESwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), e), new Swaping(),
+				new FirstNeighborImproving(e), e);
+		HillClimbing hcFSwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), f), new Swaping(),
+				new FirstNeighborImproving(f), f);
+		TSPResult res3A = hcASwapFirstCH.doAlgo(50);
+		TSPResult res3B = hcBSwapFirstCH.doAlgo(50);
+		TSPResult res3C = hcCSwapFirstCH.doAlgo(50);
+		TSPResult res3D = hcDSwapFirstCH.doAlgo(50);
+		TSPResult res3E = hcESwapFirstCH.doAlgo(50);
+		TSPResult res3F = hcFSwapFirstCH.doAlgo(50);
+		System.out.println("randomA100.tsp : ");
+		System.out.println("- RANDOM : "+ res1A.getCost());
+		System.out.println("- CONSTRUCTIVE-HEURISTIC(50) : "+ res3A.getCost());
+		System.out.println("randomB100.tsp : ");
+		System.out.println("- RANDOM : "+ res1B.getCost());
+		System.out.println("- CONSTRUCTIVE-HEURISTIC(50) : "+ res3B.getCost());
+		System.out.println("randomC100.tsp : ");
+		System.out.println("- RANDOM : "+ res1C.getCost());
+		System.out.println("- CONSTRUCTIVE-HEURISTIC(50) : "+ res3C.getCost());
+		System.out.println("randomD100.tsp : ");
+		System.out.println("- RANDOM : "+ res1D.getCost());
+		System.out.println("- CONSTRUCTIVE-HEURISTIC(50) : "+ res3D.getCost());
+		System.out.println("randomE100.tsp : ");
+		System.out.println("- RANDOM : "+ res1E.getCost());
+		System.out.println("- CONSTRUCTIVE-HEURISTIC(50) : "+ res3E.getCost());
+		System.out.println("randomF100.tsp : ");
+		System.out.println("- RANDOM : "+ res1F.getCost());
+		System.out.println("- CONSTRUCTIVE-HEURISTIC(50) : "+ res3F.getCost());
 		
 		System.out.println("------------------------------------------------------------");
-		
-		ConstructiveHeuristicInit constructiveHeuristicInit = new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), a);
-		int[] randomConstructiveHeuristic = constructiveHeuristicInit.initPathAsArray(10);
-		
-		for (int i = 0; i < randomConstructiveHeuristic.length; i++) {
-			System.out.println(randomConstructiveHeuristic[i]);
-		}
-		
-		
+		System.out.println("Question 4");
 		System.out.println("------------------------------------------------------------");
 		
+		TSPResult res4A = hcATwoOptBest.doAlgo(path);
+		TSPResult res4B = hcBTwoOptBest.doAlgo(path);
+		TSPResult res4C = hcCTwoOptBest.doAlgo(path);
+		TSPResult res4D = hcDTwoOptBest.doAlgo(path);
+		TSPResult res4E = hcETwoOptBest.doAlgo(path);
+		TSPResult res4F = hcFTwoOptBest.doAlgo(path);
 		
-		
+		System.out.println("randomA100.tsp : ");
+		System.out.println("- FIRST : "+ res2A.getCost());
+		System.out.println("- BEST : "+ res4A.getCost());
+		System.out.println("randomB100.tsp : ");
+		System.out.println("- FIRST : "+ res2B.getCost());
+		System.out.println("- BEST : "+ res4B.getCost());
+		System.out.println("randomC100.tsp : ");
+		System.out.println("- FIRST : "+ res2C.getCost());
+		System.out.println("- BEST : "+ res4C.getCost());
+		System.out.println("randomD100.tsp : ");
+		System.out.println("- FIRST : "+ res2D.getCost());
+		System.out.println("- BEST : "+ res4D.getCost());
+		System.out.println("randomE100.tsp : ");
+		System.out.println("- FIRST : "+ res2E.getCost());
+		System.out.println("- BEST : "+ res4E.getCost());
+		System.out.println("randomF100.tsp : ");
+		System.out.println("- FIRST : "+ res2F.getCost());
+		System.out.println("- BEST : "+ res4F.getCost());
+
 	}
-	
+
 }
