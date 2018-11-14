032261938a13ae3a58d5b15a8dd4ebbc70e0ac29
Nicolas DESSENNE <nico.dessenne@gmail.com>
2018-11-14 18:02:29 +0100

    ...

--- TP1/src/main/MainTP5.java
+++ TP1/src/main/MainTP5.java
@@ -29,11 +29,6 @@ public class MainTP5 {
 //			e1.printStackTrace();
 //		}
 		
-		int[] path = new int[100];
-		for (int i = 0; i < path.length; i++) {
-			path[i] = i;
-		}
-		Method.shuffleArray(path);
 		// GET FILE TSP
 		File fa = new File("randomA100.tsp");
 		File fb = new File("randomB100.tsp");
@@ -54,29 +49,28 @@ public class MainTP5 {
 		System.out.println("------------------------------------------------------------");
 		
 		try {
-			doScalar(path, a, 0.8f, 'A', b, 0.2f, 'B');
-			doScalar(path, c, 0.8f, 'C', d, 0.2f, 'D');
-			doScalar(path, e, 0.8f, 'E', f, 0.2f, 'F');
+			doScalar(a, 0.8f, 'A', b, 0.2f, 'B');
+			doScalar(c, 0.8f, 'C', d, 0.2f, 'D');
+			doScalar(e, 0.8f, 'E', f, 0.2f, 'F');
 		} catch (RandomTSPException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 	}
 
-	private static void doScalar(int[] path, RandomTSP a, float x, Character ca, RandomTSP b, float y, Character cb) throws RandomTSPException {
+	private static void doScalar(RandomTSP a, float x, Character ca, RandomTSP b, float y, Character cb) throws RandomTSPException {
 		List<Point> ps = new ArrayList<>();
 		float ax = 0f;
-		float by = 1f;
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 500; j++) {
-				Method.shuffleArray(path);
-				ps.add(Agregat.agreg(path, ax, by, a, b));
-			}
+		float by = 10f;
+		for (int i = 0; i < 100; i++) {
+			//for (int j = 0; j < 500; j++) {
+				ps.add(Agregat.agreg(a, b, Agregat.agregTSP(ax, by, a, b)));
+			//}
 			ax = ax+0.1f;
 			by = by-0.1f;
 		}
-		OffLine off = new OffLine();
-		List<Point> rps = off.doStrategy(ps);
+//		OffLine off = new OffLine();
+//		List<Point> rps = off.doStrategy(ps);
 
 		// WRITE FILE
 		System.out.println("Create file "+ca+cb+"ScalarPoints.txt");
@@ -86,7 +80,7 @@ public class MainTP5 {
 			PrintWriter printWriter = new PrintWriter(fileWriter);
 			printWriter.print("# "+ca+cb+"ScalarPoints.txt\n");
 			printWriter.print("# x\ty\n");
-			for (Point point : rps) {
+			for (Point point : /*r*/ps) {
 				printWriter.print(point.getX()+"\t"+point.getY()+"\n");
 			}
 			printWriter.close();
--- TP1/src/optimtsp/Agregat.java
+++ TP1/src/optimtsp/Agregat.java
@@ -2,17 +2,35 @@ package optimtsp;
 
 import glouton.exception.RandomTSPException;
 import glouton.obj.RandomTSP;
+import glouton.obj.TSPResult;
 import glouton.obj.services.RandomTSP.Evaluate;
+import glouton.obj.services.constructHeuristic.NearestNeighborAlgorithm;
+import hillclimbing.obj.services.HillClimbing;
+import hillclimbing.obj.services.initialization.ConstructiveHeuristicInit;
+import hillclimbing.obj.services.movement.FirstNeighborImproving;
+import hillclimbing.obj.services.neighborhood.Swaping;
+import memetic.obj.services.MemeticAlgorithm;
+import memetic.obj.services.crossover.OrderBasedCrossover;
+import memetic.obj.services.populationInit.ConstructiveHeuristicPopulation;
+import memetic.obj.services.populationInit.PopulationInitialization;
 import multi.obj.MultiTSP;
 import multi.obj.Point;
 
 public class Agregat {
 
-	public static Point agreg(int[] path, float x, float y, RandomTSP a, RandomTSP b) throws RandomTSPException {
-		return new Point(Evaluate.eval(a, path)*x, Evaluate.eval(b, path)*y);
+	public static Point agreg(RandomTSP a, RandomTSP b, MultiTSP c) throws RandomTSPException {
+		PopulationInitialization piA = new ConstructiveHeuristicPopulation(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), a));
+		OrderBasedCrossover obc = new OrderBasedCrossover();
+		HillClimbing hcASwapFirstCH = new HillClimbing(
+				new ConstructiveHeuristicInit(new NearestNeighborAlgorithm(), a), new Swaping(),
+				new FirstNeighborImproving(a), a);
+		MemeticAlgorithm maA = new MemeticAlgorithm(piA, obc, a, hcASwapFirstCH);
+		TSPResult result = maA.doAlgo(100);
+		return new Point(Evaluate.eval(a, result.getPath()), Evaluate.eval(b, result.getPath()));
 	}
 	
-	public MultiTSP agregTSP(float x, float y, RandomTSP a, RandomTSP b) {
+	public static MultiTSP agregTSP(float x, float y, RandomTSP a, RandomTSP b) {
 		float[][] agregat = new float[a.getDimension()][b.getDimension()];
 		for (int i = 0; i < agregat.length; i++) {
 			for (int j = 0; j < agregat.length; j++) {
